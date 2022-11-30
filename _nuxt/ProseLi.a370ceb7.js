c71054ea448f842eb766ff609c328d67cddcbe9d
mattkingshott <mattkingshott@users.noreply.github.com>
2022-11-30 15:49:32 +0000

    deploy: bcf86576175bb33b90b5cf1ca53f0c8217729c44

---
+++ _nuxt/ProseLi.a370ceb7.js
@@ -0,0 +1,1 @@
+import{O as o,o as r,J as s,P as t}from"./entry.05baa7f2.js";const n={};function c(e,a){return r(),s("li",null,[t(e.$slots,"default")])}const f=o(n,[["render",c]]);export{f as default};
